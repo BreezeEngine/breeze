74921702ef44c106526562f748a585231fc5d0cf
maik <maikklein@googlemail.com>
2016-04-12 09:09:44 +0200

    fps cam x rot

--- examples/opengl/source/app.d
+++ examples/opengl/source/app.d
@@ -4,10 +4,13 @@ import derelict.opengl3.gl3;
 import std.regex;
 
 struct Window{
+      import breeze.math.vector;
     GLFWwindow* window;
-    alias InputFunction = void function() nothrow;
-    InputFunction[int] inputMap;
-    this(int width, int height, string title){
+    Vec2d lastMousePos;
+    int width, height;
+    this(int _width, int _height, string title){
+        height = _height;
+        width = _width;
         DerelictGL3.load();
         glfwInit();
         // Set all the required options for GLFW
@@ -19,8 +22,6 @@ struct Window{
         // Create a GLFWwindow object that we can use for GLFW's functions
         window = glfwCreateWindow(width, height, title.ptr, null, null);
         glfwMakeContextCurrent(window);
-        glfwSetWindowUserPointer(window, cast(void*)(&this));
-        glfwSetKeyCallback(window, &callback);
         DerelictGL3.reload;
 
         // Define the viewport dimensions
@@ -28,26 +29,26 @@ struct Window{
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_CULL_FACE);
     }
-    static extern(C) void callback(GLFWwindow* window, int key, int scancode, int action, int mods) nothrow{
-        try{
-            writeln("key");
-        }
-        catch (Exception e) assert(0);
-        //Window* w = cast(Window*)glfwGetWindowUserPointer(window);
-        //InputFunction* f = key in w.inputMap;
-        //if(f != null) (*f)();
+    auto getMousePos(){
+      double x = void;
+      double y = void;
+      glfwGetCursorPos(window, &x, &y);
+      return Vec2d(x, y);
     }
-    void addInput(int key, void function() nothrow f){
-        inputMap[key] = f;
+    auto ref getLastMousePos(){
+      return lastMousePos;
     }
-    void mainLoop(void delegate() f){
+    auto getDeltaMousePos(){
+      return lastMousePos - getMousePos();
+    }
+    void mainLoop(void delegate(ref Window) f){
         while (!glfwWindowShouldClose(window)){
-            glfwPollEvents();
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            f();
-
+            f(this);
+            lastMousePos = getMousePos();
+            glfwPollEvents();
             glfwSwapBuffers(window);
         }
     }
@@ -55,51 +56,124 @@ struct Window{
         return glfwGetKey(window, key) == GLFW_PRESS;
     }
 }
+struct FPCamera{
+  import breeze.math.matrix;
+  import breeze.math.vector;
+  Window* window;
+  Vec3f position;
+  Vec3f direction;
+
+  this(Window* w, Vec3f _position, Vec3f _direction){
+    window = w;
+    position = _position;
+    direction = _direction;
+  }
+  Mat4f calcView(){
+        auto mouseDelta = window.getDeltaMousePos / window.height;
+        auto newDir = rotY((mouseDelta.x)) * Vec4f(direction, 1) ;
+        direction = newDir.xyz;
+        auto view = lookAt(position, position + direction, Vec3f(0, 1, 0));
+        return view;
+  }
+}
+struct AxisState{
+    int key;
+    float value;
+}
+struct Axis(_names...){
+    alias names = _names;
+}
+struct Action(_names...){
+    alias names = _names;
+}
+struct InputMap(Axis,Action){
+    import option;
+    import std.container: Array;
+
+    Window* window;
+    Array!AxisState[Axis.names.length] keys;
+    Array!int[Action.names.length] actionBindings;
+    void axis(string name, AxisState...)(AxisState axisStates){
+        import std.meta: staticIndexOf;
+        enum index = staticIndexOf!(name, Axis.names);
+        foreach(axisState; axisStates){
+            keys[index].insert(axisState);
+        }
+    }
+    float getAxis(string name)(){
+        import std.range;
+        import std.meta: staticIndexOf;
+        import std.algorithm.iteration: reduce;
+        enum index = staticIndexOf!(name, Axis.names);
+        float value = reduce!((acc, axisState){
+            if(window.getKey(axisState.key)){
+                return acc + axisState.value;
+            }
+            return acc;
+        })(0.0f, keys[index]);
+        import std.algorithm.comparison: max, min;
+        return value;
+    }
+    void action(string name, Keys...)(Keys keys){
+        import std.meta: staticIndexOf;
+        enum index = staticIndexOf!(name, Action.names);
+        foreach(key; keys){
+            actionBindings[index].insert(key);
+        }
+    }
+    bool getAction(string name)(){
+        import std.range;
+        import std.meta: staticIndexOf;
+        import std.algorithm.iteration: reduce;
+        enum index = staticIndexOf!(name, Action.names);
+        foreach(key; actionBindings[index]){
+            if(window.getKey(key)){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool getKey(string name)(){
+        //import std.meta: staticIndexOf;
+        //enum index = staticIndexOf!(name, names);
+        //return window.getKey(keys[index]);
+        return true;
+    }
+
+
+}
 void main()
 {
     import std.meta;
     import breeze.graphics.opengl.shader;
     import breeze.graphics.opengl.types;
     import breeze.graphics.opengl.buffer;
-    import breeze.math.vector;
+    import breeze.graphics.primitives: Cube;
     import std.conv;
     import std.range;
+    import breeze.math.vector;
     import breeze.math.matrix;
     import std.math;
+    import std.string;
 
     Window w = Window(800, 600, "Test");
-    w.addInput(GLFW_KEY_ESCAPE, ()nothrow{  printf("ecs");});
-    //DerelictGL3.load();
-    //glfwInit();
-    //// Set all the required options for GLFW
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_RESIZABLE, false);
+    auto input = InputMap!(Axis!("Forward", "Right"), Action!("Exit"))(&w);
+    input.action!"Exit"(GLFW_KEY_ESCAPE);
+    input.axis!"Forward"(
+        AxisState(GLFW_KEY_W, 1.0f),
+        AxisState(GLFW_KEY_S, -1.0f)
+    );
+    input.axis!"Right"(
+        AxisState(GLFW_KEY_D, 1.0f),
+        AxisState(GLFW_KEY_A, -1.0f)
+    );
 
-    //// Create a GLFWwindow object that we can use for GLFW's functions
-    //GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", null, null);
-    //glfwMakeContextCurrent(window);
-
-    //// Set the required callback functions
-
-    //// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
-    ////glewExperimental = GL_TRUE;
-    //// Initialize GLEW to setup the OpenGL Function pointers
-    ////glewInit();
-    //DerelictGL3.reload;
-
-    //// Define the viewport dimensions
-    //glViewport(0, 0, 800, 600);
-    //glEnable(GL_DEPTH_TEST);
-    //glEnable(GL_CULL_FACE);
     struct Vertex{
         Vec3f position;
         Vec2f uv;
     }
 
     struct Uniforms{
-        Vec3f ucolor;
         Mat4f uproj;
         Mat4f uview;
         Mat4f umodel;
@@ -117,9 +191,6 @@ void main()
         Vec4f frag_color;
     }
 
-    import breeze.graphics.opengl.shader;
-    import std.string;
-    import breeze.graphics.primitives: Cube;
 
     string vertexBody = q{
         void main(){
@@ -140,50 +211,24 @@ void main()
 
     auto shader = createTypeSafeShader(vs, fs);
 
-    Vertex[4] v2 = [
-        Vertex(Vec3f(0.5f, 0.5, 0.0f),  Vec2f(1.0f, 0.0f)),
-        Vertex(Vec3f(0.5f, -0.5, 0.0f), Vec2f(0.0f, 1.0f)),
-        Vertex(Vec3f(-0.5f, -0.5, 0.0f),Vec2f(0.0f, 0.0f)),
-        Vertex(Vec3f(-0.5f, 0.5, 0.0f), Vec2f(1.0f, 0.0f))
-    ];
-    GLuint[6] indices = [
-        0, 1, 3,  // First Triangle
-        1, 2, 3   // Second Triangle
-    ];
-    import std.container: Array;
-    Array!int indices2 = [  // Note that we start from 0!
-        0, 1, 3,  // First Triangle
-        1, 2, 3   // Second Triangle
-    ];
-
-    auto vertexBuffer = createVertexBuffer(v2);
-
-    auto vertexBuffer2 = createVertexBuffer(Cube.data);
-    auto ebo2 = createElementBuffer(Cube.indices[]);
+    auto vertexBuffer = createVertexBuffer(Cube.data);
+    auto ebo = createElementBuffer(Cube.indices[]);
 
     //auto view = Mat4f.identity;
-    auto view = lookAt(Vec3f(0, 0, -2), Vec3f(0, 0, 0), Vec3f(0, 1, 0));
     auto proj = projection(PI/2, 4.0f/3.0f, 0.1f, 100);
-    import std.range;
-    import std.conv;
-    auto ebo = createElementBuffer(indices[]);
     float frame = 0.0f;
-    w.mainLoop((){
-            draw(shader, vertexBuffer2, ebo2, DrawMode.Triangles, Uniforms(Vec3f(1, 0, 0), proj, view, rotY(frame)));
-            frame += 0.01f;
-            });
-    //while (!glfwWindowShouldClose(window)){
-    //    glfwPollEvents();
-    //    //    if (input.getKey(GLFW_KEY_ESCAPE)){
-    //    //        glfwSetWindowShouldClose(window, true);
-    //    //    }
-    //    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    //    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    //    draw(shader, vertexBuffer2, ebo2, DrawMode.Triangles, Uniforms(Vec3f(1, 0, 0), proj, view, rotY(frame)));
-    //    frame += 0.01;
-    //    //draw(shader, vertexBuffer, ebo, DrawMode.Triangles, Uniforms(Vec3f(1, 0, 0), 1.0f));
-
-    //    glfwSwapBuffers(window);
-    //}
+    glfwSetInputMode(w.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    auto cam = FPCamera(&w, Vec3f(0, 0, 2), Vec3f(0, 0, -1));
+    w.mainLoop((ref w){
+        if (input.getAction!"Exit"){
+            glfwSetWindowShouldClose(w.window, true);
+        }
+        float fwd = input.getAxis!"Forward";
+        float right = input.getAxis!"Right";
+        writeln(w.getDeltaMousePos);
+        auto view = cam.calcView();
+        //auto view = lookAt(Vec3f(0, 0, 2), Vec3f(0, 0, 0), Vec3f(0, 1, 0));
+        draw(shader, vertexBuffer, ebo, DrawMode.Triangles, Uniforms(proj, view, rotY(frame)));
+        frame += 0.01f;
+    });
 }
