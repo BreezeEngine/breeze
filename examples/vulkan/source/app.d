39b22802838a4cf87fdba98d519512f2b003d782
maik <maikklein@googlemail.com>
2016-05-23 02:24:43 +0200

    vulkan cleanup

--- examples/vulkan/source/app.d
+++ examples/vulkan/source/app.d
@@ -6,6 +6,28 @@ void enforceVk(VkResult res){
 	import std.conv;
 	enforce(res is VkResult.VK_SUCCESS, res.to!string);
 }
+VkBool32 MyDebugReportCallback(
+    VkDebugReportFlagsEXT       flags,
+    VkDebugReportObjectTypeEXT  objectType,
+    uint64_t                    object,
+    size_t                      location,
+    int32_t                     messageCode,
+    const char*                 pLayerPrefix,
+    const char*                 pMessage,
+    void*                       pUserData)
+{
+	return VK_FALSE;
+}
+
+struct VkContext{
+	VkInstance instance;
+	VkSurfaceKHR surface;
+	VkPhysicalDevice physicalDevice;
+	VkDevice logicalDevice;
+	ulong presentQueueFamilyIndex = -1;
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+}
 void main()
 {
 	import std.exception;
@@ -14,33 +36,30 @@ void main()
 	import std.algorithm.iteration;
 	import core.stdc.string;
 
+	VkContext vkcontext;
+
 	DerelictSDL2.load();
 	auto sdlWindow = SDL_CreateWindow("vulkan", 0, 0, 800, 600, 0);
 	SDL_SysWMinfo sdlWindowInfo;
 
 	SDL_VERSION(&sdlWindowInfo.version_);
 	enforce(SDL_GetWindowWMInfo(sdlWindow, &sdlWindowInfo), "sdl err");
-	writeln(sdlWindowInfo.subsystem);
 
 	DerelictErupted.load();
 	VkApplicationInfo appinfo;
 	appinfo.pApplicationName = "Breeze";
 	appinfo.apiVersion = VK_MAKE_VERSION(1, 0, 2);
 	
-	import std.container.array;
-	const(char*)[2] extensionNames = [
+	const(char*)[3] extensionNames = [
 		"VK_KHR_surface",
 		"VK_KHR_xlib_surface",
+		"VK_EXT_debug_report"
 	];
 	uint extensionCount = 0;
 	vkEnumerateInstanceExtensionProperties(null, &extensionCount, null );
-	writeln("cou ", extensionCount);
 
 	auto extensionProps = new VkExtensionProperties[](extensionCount);
 	vkEnumerateInstanceExtensionProperties(null, &extensionCount, extensionProps.ptr );
-	foreach(extension; extensionProps){
-		writeln(extension.extensionName);
-	}
 
 	enforce(extensionNames[].all!((extensionName){
 		return extensionProps[].count!((extension){
@@ -70,12 +89,17 @@ void main()
 	createinfo.enabledLayerCount = validationLayers.length;
 	createinfo.ppEnabledLayerNames = validationLayers.ptr;
 
-	VkInstance instance;
-	enforceVk(vkCreateInstance(&createinfo, null, &instance));
-	//DVulkanLoader.loadAllFunctions(instance);
-	loadInstanceLevelFunctions(instance);
-	//EruptedLoader.loadAllFunctions(instance);
+	enforceVk(vkCreateInstance(&createinfo, null, &vkcontext.instance));
+	//uint function(uint flags, VkDebugReportObjectTypeEXT objectType, ulong object, ulong location, int messageCode, const(char*) pLayerPrefix, const(char*) pMessage, void* pUserData)
 
+	loadInstanceLevelFunctions(vkcontext.instance);
+	//auto debugcallbackCreateInfo = VkDebugReportCallbackCreateInfoEXT(
+	//	VkStructureType.VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT,
+	//	null,
+	//	VkDebugReportFlagBitsEXT.VK_DEBUG_REPORT_ERROR_BIT_EXT,
+	//	&MyDebugReportCallback,
+	//	null
+	//);
 
 	auto xlibInfo = VkXlibSurfaceCreateInfoKHR(
 		VkStructureType.VK_STRUCTURE_TYPE_XLIB_SURFACE_CREATE_INFO_KHR,
@@ -85,20 +109,16 @@ void main()
 		sdlWindowInfo.info.x11.window
 	);
 	//vkCreateXcbSurfaceKHR();
-	VkSurfaceKHR surface;
-	enforceVk(vkCreateXlibSurfaceKHR(instance, &xlibInfo, null, &surface));
+	enforceVk(vkCreateXlibSurfaceKHR(vkcontext.instance, &xlibInfo, null, &vkcontext.surface));
 
 	uint numOfDevices;
-	enforceVk(vkEnumeratePhysicalDevices(instance, &numOfDevices, null));
+	enforceVk(vkEnumeratePhysicalDevices(vkcontext.instance, &numOfDevices, null));
 
 	auto devices = new VkPhysicalDevice[](numOfDevices);
-	enforceVk(vkEnumeratePhysicalDevices(instance, &numOfDevices, devices.ptr));
-
-	VkPhysicalDevice physicalDevice = null;
+	enforceVk(vkEnumeratePhysicalDevices(vkcontext.instance, &numOfDevices, devices.ptr));
 
 	const(char*)[1] deviceExtensions = ["VK_KHR_swapchain"];
 
-	size_t queueFamilyIndex = 0;
 	foreach(index, device; devices){
 		VkPhysicalDeviceProperties props;
 
@@ -109,56 +129,69 @@ void main()
 			enforce(queueCount > 0);
 			auto queueFamilyProp = new VkQueueFamilyProperties[](queueCount);
 			vkGetPhysicalDeviceQueueFamilyProperties(device, &queueCount, queueFamilyProp.ptr);
-			foreach(familyIndex,  prop; queueFamilyProp){
-				if(prop.queueCount > 0 && (prop.queueFlags & VkQueueFlagBits.VK_QUEUE_GRAPHICS_BIT)){
-					queueFamilyIndex = familyIndex;
-					physicalDevice = device;
-				}
+
+			auto presentIndex = queueFamilyProp[].countUntil!((prop){
+				return prop.queueCount > 0 && (prop.queueFlags & VkQueueFlagBits.VK_QUEUE_GRAPHICS_BIT);
+			});
+
+			VkBool32 supportsPresent;
+      vkGetPhysicalDeviceSurfaceSupportKHR(
+				device, cast(uint)presentIndex,
+				vkcontext.surface, &supportsPresent
+			);
+
+			if(presentIndex !is -1 && supportsPresent){
+				vkcontext.presentQueueFamilyIndex = presentIndex;
+				vkcontext.physicalDevice = device;
+				break;
 			}
 		}
 	}
 
+	enforce(
+		vkcontext.presentQueueFamilyIndex !is -1 &&
+		vkcontext.physicalDevice,
+		"Could not find a suitable device"
+	);
+
 	uint extensionDeviceCount = 0;
-	vkEnumerateDeviceExtensionProperties(physicalDevice, null, &extensionDeviceCount, null);
+	vkEnumerateDeviceExtensionProperties(vkcontext.physicalDevice, null, &extensionDeviceCount, null);
 	auto extensionDeviceProps = new VkExtensionProperties[](extensionDeviceCount);
 
-	vkEnumerateDeviceExtensionProperties(physicalDevice, null, &extensionDeviceCount, extensionDeviceProps.ptr);
+	vkEnumerateDeviceExtensionProperties(vkcontext.physicalDevice, null, &extensionDeviceCount, extensionDeviceProps.ptr);
 
-	enforce(physicalDevice != null, "Device is null");
+	enforce(vkcontext.physicalDevice != null, "Device is null");
 	//enforce the swapchain
 	enforce(extensionDeviceProps[].map!(prop => prop.extensionName).count!((name){
 				return strcmp(cast(const(char*))name, "VK_KHR_swapchain" ) == 0;
 	}) > 0);
 
 	float[1] priorities = [1.0f];
-	VkDeviceQueueCreateInfo deviceQueueInfo = 
+	VkDeviceQueueCreateInfo deviceQueueCreateInfo =
 		VkDeviceQueueCreateInfo(
 			VkStructureType.VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
 			null,
 			0,
-			cast(uint)queueFamilyIndex,
+			cast(uint)vkcontext.presentQueueFamilyIndex,
 			cast(uint)priorities.length,
 			priorities.ptr
+	);
 
-		);
 	auto deviceInfo = VkDeviceCreateInfo(
 		VkStructureType.VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
 		null,
 		0,
 		1,
-		&deviceQueueInfo,
+		&deviceQueueCreateInfo,
 		0,
 		null,
 		cast(uint)deviceExtensions.length,
 		deviceExtensions.ptr,
 		null
 	);
-	VkDevice device;
-	enforceVk(vkCreateDevice(physicalDevice, &deviceInfo, null, &device));
+	enforceVk(vkCreateDevice(vkcontext.physicalDevice, &deviceInfo, null, &vkcontext.logicalDevice));
 
-	loadDeviceLevelFunctions(device);
+	loadDeviceLevelFunctions(vkcontext.logicalDevice);
 	VkQueue queue;
-	vkGetDeviceQueue(device, cast(uint)queueFamilyIndex, 0, &queue);
-
-
+	vkGetDeviceQueue(vkcontext.logicalDevice, cast(uint)vkcontext.presentQueueFamilyIndex, 0, &vkcontext.graphicsQueue);
 }
